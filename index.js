4d7b0ebc33e2fb3dd27292220a17134fa269e329
kerrykiambi-lab254 <kerry.kiambi@student.moringaschool.com>
2026-03-26 21:02:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
-//Write your code here
+// index.js
 
+// Attendee object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+/**
+ * Logs the name of the attendee.
+ * @param {Object} person - The attendee object.
+ */
+function logAttendeeName(person) {
+  console.log(person.name);
+}
 
-//Needed for the tests to work. Don't modify
+/**
+ * Logs the ticket price of the attendee.
+ * @param {Object} person - The attendee object.
+ */
+function logTicketPrice(person) {
+  console.log(person.ticketPrice);
+}
+
+/**
+ * Updates the ticket type of the attendee.
+ * @param {Object} person - The attendee object.
+ * @param {string} newType - The new ticket type.
+ */
+function updateTicketType(person, newType) {
+  person.ticketType = newType;
+}
+
+/**
+ * Updates the ticket price of the attendee.
+ * @param {Object} person - The attendee object.
+ * @param {number} newPrice - The new ticket price.
+ */
+function updateTicketPrice(person, newPrice) {
+  person.ticketPrice = newPrice;
+}
+
+/**
+ * Removes the event property from the attendee object.
+ * @param {Object} person - The attendee object.
+ */
+function removeEventProperty(person) {
+  delete person.event;
+}
+
+/**
+ * Adds a checkedIn property set to true on the attendee object.
+ * @param {Object} person - The attendee object.
+ */
+function addCheckedInProperty(person) {
+  person.checkedIn = true;
+}
+
 module.exports = {
-  ...(typeof attendee !== 'undefined' && { attendee }),
-  ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
-  ...(typeof logTicketPrice !== 'undefined' && { logTicketPrice }),
-  ...(typeof updateTicketType !== 'undefined' && { updateTicketType }),
-  ...(typeof updateTicketPrice !== 'undefined' && { updateTicketPrice }),
-  ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
-  ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
+  attendee,
+  logAttendeeName,
+  logTicketPrice,
+  updateTicketType,
+  updateTicketPrice,
+  removeEventProperty,
+  addCheckedInProperty
 };
